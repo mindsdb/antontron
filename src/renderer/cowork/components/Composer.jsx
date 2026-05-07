47b3803734094fc508453cd038803c3b546529ca
Minura Punchihewa <minurapunchihewa17@gmail.com>
2026-05-07 12:25:10 -0700

    updated the composer to allow any file type

--- src/renderer/cowork/components/Composer.jsx
+++ src/renderer/cowork/components/Composer.jsx
@@ -262,7 +262,6 @@ export default function Composer({
         ref={fileRef}
         type="file"
         multiple
-        accept="*/*,image/*"
         hidden
         onChange={(event) => handleAttachFiles(event.target.files)}
       />
